25e280f11bf0d004907597576b5e6c4ca9964e9f
Mariana A <mariana.antoniohdz@gmail.com>
2026-08-07 11:29:02 -0600

    Actualizar URL de API a Railway

--- backend/-- SQLite.sql
+++ backend/-- SQLite.sql
@@ -20,3 +20,22 @@ PRAGMA table_info(Alumnos);
 DROP TABLE Alumnos;
 
 PRAGMA table_info(Alumnos);
+
+PRAGMA foreign_keys = ON;
+
+CREATE TABLE IF NOT EXISTS Alumnos (
+    Matricula TEXT PRIMARY KEY,
+    idCarrera INTEGER NOT NULL,
+    idDatosP INTEGER NOT NULL,
+    Status TEXT DEFAULT 'A',
+    FOREIGN KEY (idCarrera) REFERENCES Carreras(idCarrera) ON DELETE RESTRICT ON UPDATE CASCADE,
+    FOREIGN KEY (idDatosP) REFERENCES DatosPersonales(idDatosP) ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+
+SELECT name 
+FROM sqlite_master 
+WHERE type = 'table' AND name NOT LIKE 'sqlite_%'
+ORDER BY name;
+
+SELECT * FROM Alumnos;
